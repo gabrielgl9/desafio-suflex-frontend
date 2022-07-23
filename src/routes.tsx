fcfd8c7bf6c946cfee28d259a03745a84ef84142
Gabriel Viegas <gabrielgl13@hotmail.com>
2022-07-23 02:04:28 -0300

    feat: add guard route for favorite characters

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Detail from './pages/detail/detail'
+import FavoriteCharacters from './pages/favorite-characters/favorite-characters'
 import Home from './pages/home/home'
 import Login from './pages/login/login'
 import Logout from './pages/logout'
 import Register from './pages/register/register'
+import { isAuthenticated } from './services/auth'
 
 const Routing = () => {
   return (
@@ -14,6 +16,10 @@ const Routing = () => {
         <Route element={<Login />} path="/login" />
         <Route element={<Logout />} path="/logout" />
         <Route element={<Register />} path="/register" />
+        <Route
+          element={isAuthenticated() ? <FavoriteCharacters /> : <Home />}
+          path={isAuthenticated() ? '/favorite-characters' : '/'}
+        />
       </Routes>
     </BrowserRouter>
   )
